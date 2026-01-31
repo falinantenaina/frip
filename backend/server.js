95c473a741b422971fc6c5e6dc91305cec51f850
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-01-31 16:02:13 +0300

    --amend

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,9 @@ app.use(
       "https://livefrip.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
 app.use(express.json());
